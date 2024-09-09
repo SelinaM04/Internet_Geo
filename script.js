c3dad8e79b90a8c0085ad2fc3daafeb651562d4b
SelinaM04 <may162@newschool.edu>
2024-09-09 17:12:32 -0400

    9-9

---
+++ script.js
@@ -0,0 +1,21 @@
+let isComplete = false;
+let testVal;
+
+if (!isComplete) {
+    testVal = true;
+}
+
+/*-----*/
+
+let number4 = 'Vision in this technological feast becomes unregulated gluttony; all seems not just mythically about the god trick of seeing everything from nowhere, but to have put the myth into ordinary practice. And like the god trick, this eye fucks the world to make techno-monsters.';
+
+console.log(number4.length);
+
+/*-----*/
+
+/* Using the let kywords because it can rearrange the value for the variabe */
+
+let string1 = 'We cannot think of a time that is oceanless';
+let string2 = 'Or of an ocean not littered with wastage';
+
+let 
